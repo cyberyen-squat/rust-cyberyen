db37aa865d88b266b15581eb55eb3833c99b0156
barrystyle <barrystyle@westnet.com.au>
2025-04-14 16:17:53 +0800

    update constants.rs to suit cyberyen

--- src/blockdata/constants.rs
+++ src/blockdata/constants.rs
@@ -36,11 +36,11 @@ pub const MAX_SEQUENCE: u32 = 0xFFFFFFFF;
 /// How many satoshis are in "one bitcoin"
 pub const COIN_VALUE: u64 = 100_000_000;
 /// How many seconds between blocks we expect on average
-pub const TARGET_BLOCK_SPACING: u32 = 150;
+pub const TARGET_BLOCK_SPACING: u32 = 60;
 /// How many blocks between diffchanges
-pub const DIFFCHANGE_INTERVAL: u32 = 2016;
+pub const DIFFCHANGE_INTERVAL: u32 = 240;
 /// How much time on average should occur between diffchanges
-pub const DIFFCHANGE_TIMESPAN: u32 = 150 * 2016;
+pub const DIFFCHANGE_TIMESPAN: u32 = 60 * 240;
 /// The maximum allowed weight for a block, see BIP 141 (network rule)
 pub const MAX_BLOCK_WEIGHT: u32 = 4_000_000;
 /// The minimum transaction weight for a valid serialized transaction
@@ -50,17 +50,17 @@ pub const WITNESS_SCALE_FACTOR: usize = 4;
 /// The maximum allowed number of signature check operations in a block
 pub const MAX_BLOCK_SIGOPS_COST: i64 = 80_000;
 /// Mainnet (bitcoin) pubkey address prefix.
-pub const PUBKEY_ADDRESS_PREFIX_MAIN: u8 = 48; // 0x30
+pub const PUBKEY_ADDRESS_PREFIX_MAIN: u8 = 28; // 0x1c
 /// Mainnet (bitcoin) script address prefix.
-pub const SCRIPT_ADDRESS_PREFIX_MAIN: u8 = 50; // 0x32
+pub const SCRIPT_ADDRESS_PREFIX_MAIN: u8 = 22; // 0x16
 /// Test (testnet, signet, regtest) pubkey address prefix.
-pub const PUBKEY_ADDRESS_PREFIX_TEST: u8 = 111; // 0x6f
+pub const PUBKEY_ADDRESS_PREFIX_TEST: u8 = 112; // 0x70
 /// Test (testnet, signet, regtest) script address prefix.
 pub const SCRIPT_ADDRESS_PREFIX_TEST: u8 = 58; // 0x3a
 /// The maximum allowed script size.
 pub const MAX_SCRIPT_ELEMENT_SIZE: usize = 520;
 /// How may blocks between halvings.
-pub const SUBSIDY_HALVING_INTERVAL: u32 = 840_000;
+pub const SUBSIDY_HALVING_INTERVAL: u32 = 100_000;
 
 /// In Bitcoind this is insanely described as ~((u256)0 >> 32)
 pub fn max_target(_: Network) -> Uint256 {
@@ -71,7 +71,7 @@ pub fn max_target(_: Network) -> Uint256 {
 /// since keeping everything below this value should prevent overflows
 /// if you are doing anything remotely sane with monetary values).
 pub fn max_money(_: Network) -> u64 {
-    84_000_000 * COIN_VALUE
+    10_000_000_000 * COIN_VALUE
 }
 
 /// Constructs and returns the coinbase (and only) transaction of the Bitcoin genesis block
@@ -89,7 +89,7 @@ fn bitcoin_genesis_tx() -> Transaction {
     // Inputs
     let in_script = script::Builder::new().push_scriptint(486604799)
                                           .push_scriptint(4)
-                                          .push_slice("NY Times 05/Oct/2011 Steve Jobs, Apple’s Visionary, Dies at 56".as_bytes())
+                                          .push_slice("The Dark Side of the Moon 2022".as_bytes())
                                           .into_script();
     ret.input.push(TxIn {
         previous_output: OutPoint::null(),
@@ -107,7 +107,7 @@ fn bitcoin_genesis_tx() -> Transaction {
         .push_opcode(opcodes::all::OP_CHECKSIG)
         .into_script();
     ret.output.push(TxOut {
-        value: 50 * COIN_VALUE,
+        value: 1000000 * COIN_VALUE,
         script_pubkey: out_script
     });
 
@@ -127,9 +127,9 @@ pub fn genesis_block(network: Network) -> Block {
                     version: 1,
                     prev_blockhash: Default::default(),
                     merkle_root,
-                    time: 1317972665,
+                    time: 1659708882,
                     bits: 0x1e0ffff0,
-                    nonce: 2084524493
+                    nonce: 2000096520
                 },
                 txdata,
                 mweb_block: None
@@ -141,9 +141,9 @@ pub fn genesis_block(network: Network) -> Block {
                     version: 1,
                     prev_blockhash: Default::default(),
                     merkle_root,
-                    time: 1486949366,
+                    time: 1744018230,
                     bits: 0x1e0ffff0,
-                    nonce: 293345
+                    nonce: 245836
                 },
                 txdata,
                 mweb_block: None
@@ -169,9 +169,9 @@ pub fn genesis_block(network: Network) -> Block {
                     version: 1,
                     prev_blockhash: Default::default(),
                     merkle_root,
-                    time: 1296688602,
+                    time: 1659708882,
                     bits: 0x207fffff,
-                    nonce: 2
+                    nonce: 4
                 },
                 txdata,
                 mweb_block: None
@@ -198,17 +198,17 @@ mod test {
         assert_eq!(gen.input[0].previous_output.txid, Default::default());
         assert_eq!(gen.input[0].previous_output.vout, 0xFFFFFFFF);
         assert_eq!(serialize(&gen.input[0].script_sig),
-                   Vec::from_hex("4804ffff001d0104404e592054696d65732030352f4f63742f32303131205374657665204a6f62732c204170706c65e280997320566973696f6e6172792c2044696573206174203536").unwrap());
+                   Vec::from_hex("2604ffff001d01041e546865204461726b2053696465206f6620746865204d6f6f6e2032303232").unwrap());
 
         assert_eq!(gen.input[0].sequence, MAX_SEQUENCE);
         assert_eq!(gen.output.len(), 1);
         assert_eq!(serialize(&gen.output[0].script_pubkey),
-                   Vec::from_hex("4341040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9ac").unwrap());
-        assert_eq!(gen.output[0].value, 50 * COIN_VALUE);
+                   Vec::from_hex("41040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9ac").unwrap());
+        assert_eq!(gen.output[0].value, 1000000 * COIN_VALUE);
         assert_eq!(gen.lock_time, 0);
 
         assert_eq!(format!("{:x}", gen.wtxid()),
-                   "97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9".to_string());
+                   "7055a6ae72bdfdaafaac2619d61684e037f477870d70951bd3bf6964d36bc405".to_string());
     }
 
     #[test]
@@ -218,12 +218,12 @@ mod test {
         assert_eq!(gen.header.version, 1);
         assert_eq!(gen.header.prev_blockhash, Default::default());
         assert_eq!(format!("{:x}", gen.header.merkle_root),
-                   "97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9".to_string());
-        assert_eq!(gen.header.time, 1317972665);
+                   "7055a6ae72bdfdaafaac2619d61684e037f477870d70951bd3bf6964d36bc405".to_string());
+        assert_eq!(gen.header.time, 1659708882);
         assert_eq!(gen.header.bits, 0x1e0ffff0);
-        assert_eq!(gen.header.nonce, 2084524493);
+        assert_eq!(gen.header.nonce, 2000096520);
         assert_eq!(format!("{:x}", gen.header.block_hash()),
-                   "12a765e31ffd4059bada1e25190f6e98c99d9714d334efa41a195a7e7e04bfe2".to_string());
+                   "34458c96bb547193fa90b2f2599056684b0083d8a2996f2025943eb545031d29".to_string());
     }
 
     #[test]
@@ -232,12 +232,12 @@ mod test {
         assert_eq!(gen.header.version, 1);
         assert_eq!(gen.header.prev_blockhash, Default::default());
         assert_eq!(format!("{:x}", gen.header.merkle_root),
-                  "97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9".to_string());
-        assert_eq!(gen.header.time, 1486949366);
+                  "7055a6ae72bdfdaafaac2619d61684e037f477870d70951bd3bf6964d36bc405".to_string());
+        assert_eq!(gen.header.time, 1744018230);
         assert_eq!(gen.header.bits, 0x1e0ffff0);
-        assert_eq!(gen.header.nonce, 293345);
+        assert_eq!(gen.header.nonce, 245836);
         assert_eq!(format!("{:x}", gen.header.block_hash()),
-                   "4966625a4b2851d9fdee139e56211a0d88575f59ed816ff5e6a63deb4e3e29a0".to_string());
+                   "1f847ccbaa8055eb1af82aef862aeb53d1d4d3ff81a8bcc514f6c071a4e6e580".to_string());
     }
 
     #[test]
